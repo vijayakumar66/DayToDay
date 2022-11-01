432232de764d1e23d26032ecd202812870a6ebe9
vijayakumar66 <109418314+vijayakumar66@users.noreply.github.com>
2022-11-01 12:50:02 +0530

    Update SubSequnces.java

--- 31-10-2022/SubSequnces.java
+++ 31-10-2022/SubSequnces.java
@@ -1,78 +1,62 @@
 import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
-public class NextGrerater {
-	public static class Node
-	{
-		private Node next;
-		private int value;
-		public Node(int item) {
-			this.value=item;
-		}
-		 public static Node createList(int[] arr) {
-			Node head=new Node(arr[0]);
-			Node current=head;
-			for(int i=1;i<arr.length;i++)
-			{
-				current.next=new Node(arr[i]);
-				current=current.next;
-			}
-			return head;
-		}
-	}
-	public int[] greaterNode(Node head)
-	{
-		int length=findLength(head);
-		int[] res=new int[length];
-		int index=0;
-		while(head.next!=null)
-		{
-			int jump=0;
-			int greater=0;
-			boolean isFound=false;
-			
-			Node tem=head.next;
-			while(tem!=null)
-			{
-				jump++;
-				if(tem.value>head.value)
-				{
-					greater=tem.value;
-					isFound=true;
-					break;
-				}
-				tem=tem.next;
-			}
-			if(!isFound) {
-				res[index]=0;
-				index++;
-				head=head.next;
-			}
-			else {
-				for(int i=0;i<jump;i++)
-				{
-					res[index]=greater;
-					index++;
-					head=head.next;
-				}
-			}
-		}
-		return res;
-	}
-	private int findLength(NextGrerater.Node head) {
-		Node current=head;
-		int count=0;
-		while(current!=null) {
-			current=current.next;
-			count++;
-		}
-		//System.out.println(count);
-		return count;
-	}
+public class SubSquence {
+	
 	public static void main(String[] args) {
-		int[] arr= {2,7,4,3,5};
-		Node head=Node.createList(arr);
-		System.out.println(Arrays.toString(new NextGrerater().greaterNode(head)) );
+		//String s="XYBYAXBY";
+		String s="ABAB";
+		//String s="ABACDB";
+		new SubSquence().numOfSubSquence(s);
 	}
+	 String comb="";
+	 Set<String> res;
+	
+	public  void numOfSubSquence(String s)
+	{
+		res=new HashSet<>();
+		subSquence(s,0);
+		System.out.println(res);
+		System.out.println(res.size());
+		
+	}
+	 void subSquence(String str,int index) {
+		
+		if(comb.length()>1) {
+			checkContain(comb,str.substring(index-1));
+		}
+		for(int i=index;i<str.length();i++)
+		{
+			comb+=str.charAt(i);
+			subSquence(str,i+1);
+			comb=comb.substring(0,comb.length()-1);
+		}
+		
+	}
+	  void checkContain(String comsub,String sub) {
+		 int len=comsub.length();
+		 boolean isMatch=true;
+		 int index1=-1;
+		 for(int i=0;i<len;i++)
+		 {
+			 sub=sub.substring(index1+1);
 
+			 //System.out.println(comsub.charAt(i));
+			 index1=sub.indexOf(comsub.charAt(i));
+			 if(index1==-1)
+			 {
+				 isMatch=false;
+				 break;
+			 }
+		 }
+		 if(isMatch)
+		 {
+			 System.out.println(comsub);
+			res.add(comsub); 
+			return;
+		 }
+
+	}
 }
